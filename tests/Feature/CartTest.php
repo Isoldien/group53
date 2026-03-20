16324dd36bf079b4fd560beee11c00e5719eadf9
Judy Johnson <Judy2solo@gmail.com>
2026-03-20 17:31:41 +0000

    Update cart test: rename test and improve assertions

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -4,22 +4,25 @@ use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
 
-it('allows users to add products to their cart', function () {
+it("add a product to a users cart", function () {
 
     $user = User::factory()->create();
-    $product = Product::first() ??  Product::factory()->create();
+    $product = Product::factory()->create();
 
+    $cart = Cart::factory()->create([
+        'user_id' => $user->user_id,
+    ]);
+
+    
     $response = $this -> actingAs($user)
         -> post(route('cart.add',[
             'product_id' => $product -> product_id, 
             'quantity' => 1
         ]));
 
-    // Expectations and Assertions
-    $cart = Cart::where('user_id', $user -> user_id)->first();
-
-    expect($cart)->not->toBeNull(); // Checks if user's cart exists
-    $this->assertDatabaseHas('cart_items',[ // Checks if product exists in user's cart
+    // Assertions - checks if product assigned to right user
+    $response->assertRedirect();
+    $this->assertDatabaseHas('cart_items',[
         'product_id' => $product -> product_id,
         'cart_id' => $cart -> cart_id
     ]);
