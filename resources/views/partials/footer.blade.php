8f94d9653de143b40002c1eddf519ee35e84541e
Habibur Rahman <240217006@aston.ac.uk@>
2026-03-21 13:03:17 +0000

    refactor: change redundant gradient logo in footer to reflect website theme

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -35,13 +35,4 @@
 <div class="mt-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-orange-500 bg-[length:200%_200%] animate-[gradient_5s_ease-in-out_infinite]" style="background-size:200% 200%;">
   YouZoo
 </div>
-<style>
-@keyframes gradient {
-  0% { background-position: 0% 50%; }
-  50% { background-position: 100% 50%; }
-  100% { background-position: 0% 50%; }
-}
-</style>
-        </div>
-    </div>
 </footer>
